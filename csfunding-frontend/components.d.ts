b4a83167f1f13b977d11f4615571fb4441a47592
5egeg6yryb <100212619+5egeg6yryb@users.noreply.github.com>
2023-04-09 11:36:14 +0800

    create Front-end home page

--- csfunding-frontend/components.d.ts
+++ csfunding-frontend/components.d.ts
@@ -12,6 +12,7 @@ declare module '@vue/runtime-core' {
     ElAside: typeof import('element-plus/es')['ElAside']
     ElAvatar: typeof import('element-plus/es')['ElAvatar']
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
     ElCol: typeof import('element-plus/es')['ElCol']
     ElContainer: typeof import('element-plus/es')['ElContainer']
     ElDropdown: typeof import('element-plus/es')['ElDropdown']
